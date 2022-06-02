bf4e9a65798e89a893912bcc013d7e58060c5607
rejohnbd <rejohn.cse@gmail.com>
2022-06-02 16:44:24 +0600

    Initalize Default Layout Page

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,18 @@ import { createRouter, createWebHistory } from "vue-router";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
 import Dashboard from "../views/Dashboard.vue";
+import DefaultLayout from "../components/DefaultLayout.vue";
+import Surveys from "../views/Surveys.vue";
 
 const routes = [
   {
     path: "/",
-    name: "Dashboar",
-    component: Dashboard,
+    redirect: "/dashboard",
+    component: DefaultLayout,
+    children: [
+      { path: "/dashboard", name: "Dashboard", component: Dashboard },
+      { path: "/surveys", name: "Surveys", component: Surveys },
+    ],
   },
   {
     path: "/login",

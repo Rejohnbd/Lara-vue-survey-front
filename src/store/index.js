81319a694ca1a7cd4db667c46ea6b7673e95d32f
rejohnbd <rejohn.cse@gmail.com>
2022-06-09 03:06:28 +0600

    Crate Logout method and clear store

--- src/store/index.js
+++ src/store/index.js
@@ -22,11 +22,19 @@ const store = createStore({
         return data;
       });
     },
+    logout({ commit }) {
+      console.log("here");
+      return axiosClient.post("/logout").then((response) => {
+        commit("logout");
+        return response;
+      });
+    },
   },
   mutations: {
     logout: (state) => {
       state.user.data = {};
       state.user.token = null;
+      sessionStorage.removeItem("TOKEN");
     },
     setUser: (state, userData) => {
       state.user.token = userData.token;

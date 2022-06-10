16ab184f8e8cee8598077fad5249521d0c1a4155
rejohnbd <rejohn.cse@gmail.com>
2022-06-11 02:43:29 +0600

    Define db structure, create survey page with card

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import store from "../store";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
 import Dashboard from "../views/Dashboard.vue";
+import SurveyView from "../views/SurveyView.vue";
 import DefaultLayout from "../components/DefaultLayout.vue";
 import AuthLayout from "../components/AuthLayout.vue";
 import Surveys from "../views/Surveys.vue";
@@ -17,6 +18,8 @@ const routes = [
     children: [
       { path: "/dashboard", name: "Dashboard", component: Dashboard },
       { path: "/surveys", name: "Surveys", component: Surveys },
+      { path: "/surveys/create", name: "SurveyCreate", component: SurveyView },
+      { path: "/surveys/:id", name: "SurveyView", component: SurveyView },
     ],
   },
   {
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,648 @@
 import { createStore } from "vuex";
 import axiosClient from "../axios";
 
+const tmpSurveys = [
+  {
+    id: 100,
+    title: "The Rejohn",
+    slug: "the-rejohn",
+    status: "draft",
+    image:
+      "https://i.picsum.photos/id/260/200/200.jpg?hmac=Nu9V4Ixqq3HiFhfkcsL5mNRZAZyEHG2jotmiiMRdxGA",
+    description: "sdfa dadfjadf fajsdflaksd fajdfakdlf ffjadsfjkasdf",
+    created_at: "2021-12-20 18:00:00",
+    updated_at: "2021-12-20 18:00:00",
+    expire_at: "2021-12-20 18:00:00",
+    question: [
+      {
+        id: 1,
+        type: "select",
+        question: "From wich country are you?",
+        description: null,
+        data: {
+          options: [
+            {
+              uuid: "f8af96",
+              text: "USA",
+            },
+            {
+              uuid: "f8af97",
+              text: "Georgia",
+            },
+            {
+              uuid: "f8af98",
+              text: "Germany",
+            },
+            {
+              uuid: "f8af96",
+              text: "Bangladesh",
+            },
+          ],
+        },
+      },
+      {
+        id: 2,
+        type: "checkbox",
+        question: "From wich country are you?",
+        description: null,
+        data: {
+          options: [
+            {
+              uuid: "f8af96",
+              text: "USA",
+            },
+            {
+              uuid: "f8af97",
+              text: "Georgia",
+            },
+            {
+              uuid: "f8af98",
+              text: "Germany",
+            },
+            {
+              uuid: "f8af96",
+              text: "Bangladesh",
+            },
+          ],
+        },
+      },
+      {
+        id: 3,
+        type: "checkbox",
+        question: "From wich country are you?",
+        description: null,
+        data: {
+          options: [
+            {
+              uuid: "f8af96",
+              text: "USA",
+            },
+            {
+              uuid: "f8af97",
+              text: "Georgia",
+            },
+            {
+              uuid: "f8af98",
+              text: "Germany",
+            },
+            {
+              uuid: "f8af96",
+              text: "Bangladesh",
+            },
+          ],
+        },
+      },
+      {
+        id: 1,
+        type: "radio",
+        question: "From wich country are you?",
+        description: null,
+        data: {
+          options: [
+            {
+              uuid: "f8af96",
+              text: "USA",
+            },
+            {
+              uuid: "f8af97",
+              text: "Georgia",
+            },
+            {
+              uuid: "f8af98",
+              text: "Germany",
+            },
+            {
+              uuid: "f8af96",
+              text: "Bangladesh",
+            },
+          ],
+        },
+      },
+      {
+        id: 1,
+        type: "checkbox",
+        question: "From wich country are you?",
+        description: null,
+        data: {
+          options: [
+            {
+              uuid: "f8af96",
+              text: "USA",
+            },
+            {
+              uuid: "f8af97",
+              text: "Georgia",
+            },
+            {
+              uuid: "f8af98",
+              text: "Germany",
+            },
+            {
+              uuid: "f8af96",
+              text: "Bangladesh",
+            },
+          ],
+        },
+      },
+      {
+        id: 6,
+        type: "text",
+        question: "What's your favourite youtueb channel",
+        description: null,
+        data: {},
+      },
+      {
+        id: 7,
+        type: "textarea",
+        question: "What's your favourite youtueb channel",
+        description: null,
+        data: {},
+      },
+    ],
+  },
+  {
+    id: 200,
+    title: "The Rejohn",
+    slug: "the-rejohn",
+    status: "draft",
+    image:
+      "https://i.picsum.photos/id/260/200/200.jpg?hmac=Nu9V4Ixqq3HiFhfkcsL5mNRZAZyEHG2jotmiiMRdxGA",
+    description: "sdfa dadfjadf fajsdflaksd fajdfakdlf ffjadsfjkasdf",
+    created_at: "2021-12-20 18:00:00",
+    updated_at: "2021-12-20 18:00:00",
+    expire_at: "2021-12-20 18:00:00",
+    question: [
+      {
+        id: 1,
+        type: "select",
+        question: "From wich country are you?",
+        description: null,
+        data: {
+          options: [
+            {
+              uuid: "f8af96",
+              text: "USA",
+            },
+            {
+              uuid: "f8af97",
+              text: "Georgia",
+            },
+            {
+              uuid: "f8af98",
+              text: "Germany",
+            },
+            {
+              uuid: "f8af96",
+              text: "Bangladesh",
+            },
+          ],
+        },
+      },
+      {
+        id: 2,
+        type: "checkbox",
+        question: "From wich country are you?",
+        description: null,
+        data: {
+          options: [
+            {
+              uuid: "f8af96",
+              text: "USA",
+            },
+            {
+              uuid: "f8af97",
+              text: "Georgia",
+            },
+            {
+              uuid: "f8af98",
+              text: "Germany",
+            },
+            {
+              uuid: "f8af96",
+              text: "Bangladesh",
+            },
+          ],
+        },
+      },
+      {
+        id: 3,
+        type: "checkbox",
+        question: "From wich country are you?",
+        description: null,
+        data: {
+          options: [
+            {
+              uuid: "f8af96",
+              text: "USA",
+            },
+            {
+              uuid: "f8af97",
+              text: "Georgia",
+            },
+            {
+              uuid: "f8af98",
+              text: "Germany",
+            },
+            {
+              uuid: "f8af96",
+              text: "Bangladesh",
+            },
+          ],
+        },
+      },
+      {
+        id: 1,
+        type: "radio",
+        question: "From wich country are you?",
+        description: null,
+        data: {
+          options: [
+            {
+              uuid: "f8af96",
+              text: "USA",
+            },
+            {
+              uuid: "f8af97",
+              text: "Georgia",
+            },
+            {
+              uuid: "f8af98",
+              text: "Germany",
+            },
+            {
+              uuid: "f8af96",
+              text: "Bangladesh",
+            },
+          ],
+        },
+      },
+      {
+        id: 1,
+        type: "checkbox",
+        question: "From wich country are you?",
+        description: null,
+        data: {
+          options: [
+            {
+              uuid: "f8af96",
+              text: "USA",
+            },
+            {
+              uuid: "f8af97",
+              text: "Georgia",
+            },
+            {
+              uuid: "f8af98",
+              text: "Germany",
+            },
+            {
+              uuid: "f8af96",
+              text: "Bangladesh",
+            },
+          ],
+        },
+      },
+      {
+        id: 6,
+        type: "text",
+        question: "What's your favourite youtueb channel",
+        description: null,
+        data: {},
+      },
+      {
+        id: 7,
+        type: "textarea",
+        question: "What's your favourite youtueb channel",
+        description: null,
+        data: {},
+      },
+    ],
+  },
+  {
+    id: 300,
+    title: "The Rejohn",
+    slug: "the-rejohn",
+    status: "draft",
+    image:
+      "https://i.picsum.photos/id/260/200/200.jpg?hmac=Nu9V4Ixqq3HiFhfkcsL5mNRZAZyEHG2jotmiiMRdxGA",
+    description: "sdfa dadfjadf fajsdflaksd fajdfakdlf ffjadsfjkasdf",
+    created_at: "2021-12-20 18:00:00",
+    updated_at: "2021-12-20 18:00:00",
+    expire_at: "2021-12-20 18:00:00",
+    question: [
+      {
+        id: 1,
+        type: "select",
+        question: "From wich country are you?",
+        description: null,
+        data: {
+          options: [
+            {
+              uuid: "f8af96",
+              text: "USA",
+            },
+            {
+              uuid: "f8af97",
+              text: "Georgia",
+            },
+            {
+              uuid: "f8af98",
+              text: "Germany",
+            },
+            {
+              uuid: "f8af96",
+              text: "Bangladesh",
+            },
+          ],
+        },
+      },
+      {
+        id: 2,
+        type: "checkbox",
+        question: "From wich country are you?",
+        description: null,
+        data: {
+          options: [
+            {
+              uuid: "f8af96",
+              text: "USA",
+            },
+            {
+              uuid: "f8af97",
+              text: "Georgia",
+            },
+            {
+              uuid: "f8af98",
+              text: "Germany",
+            },
+            {
+              uuid: "f8af96",
+              text: "Bangladesh",
+            },
+          ],
+        },
+      },
+      {
+        id: 3,
+        type: "checkbox",
+        question: "From wich country are you?",
+        description: null,
+        data: {
+          options: [
+            {
+              uuid: "f8af96",
+              text: "USA",
+            },
+            {
+              uuid: "f8af97",
+              text: "Georgia",
+            },
+            {
+              uuid: "f8af98",
+              text: "Germany",
+            },
+            {
+              uuid: "f8af96",
+              text: "Bangladesh",
+            },
+          ],
+        },
+      },
+      {
+        id: 1,
+        type: "radio",
+        question: "From wich country are you?",
+        description: null,
+        data: {
+          options: [
+            {
+              uuid: "f8af96",
+              text: "USA",
+            },
+            {
+              uuid: "f8af97",
+              text: "Georgia",
+            },
+            {
+              uuid: "f8af98",
+              text: "Germany",
+            },
+            {
+              uuid: "f8af96",
+              text: "Bangladesh",
+            },
+          ],
+        },
+      },
+      {
+        id: 1,
+        type: "checkbox",
+        question: "From wich country are you?",
+        description: null,
+        data: {
+          options: [
+            {
+              uuid: "f8af96",
+              text: "USA",
+            },
+            {
+              uuid: "f8af97",
+              text: "Georgia",
+            },
+            {
+              uuid: "f8af98",
+              text: "Germany",
+            },
+            {
+              uuid: "f8af96",
+              text: "Bangladesh",
+            },
+          ],
+        },
+      },
+      {
+        id: 6,
+        type: "text",
+        question: "What's your favourite youtueb channel",
+        description: null,
+        data: {},
+      },
+      {
+        id: 7,
+        type: "textarea",
+        question: "What's your favourite youtueb channel",
+        description: null,
+        data: {},
+      },
+    ],
+  },
+  {
+    id: 400,
+    title: "The Rejohn",
+    slug: "the-rejohn",
+    status: "draft",
+    image:
+      "https://i.picsum.photos/id/260/200/200.jpg?hmac=Nu9V4Ixqq3HiFhfkcsL5mNRZAZyEHG2jotmiiMRdxGA",
+    description: "sdfa dadfjadf fajsdflaksd fajdfakdlf ffjadsfjkasdf",
+    created_at: "2021-12-20 18:00:00",
+    updated_at: "2021-12-20 18:00:00",
+    expire_at: "2021-12-20 18:00:00",
+    question: [
+      {
+        id: 1,
+        type: "select",
+        question: "From wich country are you?",
+        description: null,
+        data: {
+          options: [
+            {
+              uuid: "f8af96",
+              text: "USA",
+            },
+            {
+              uuid: "f8af97",
+              text: "Georgia",
+            },
+            {
+              uuid: "f8af98",
+              text: "Germany",
+            },
+            {
+              uuid: "f8af96",
+              text: "Bangladesh",
+            },
+          ],
+        },
+      },
+      {
+        id: 2,
+        type: "checkbox",
+        question: "From wich country are you?",
+        description: null,
+        data: {
+          options: [
+            {
+              uuid: "f8af96",
+              text: "USA",
+            },
+            {
+              uuid: "f8af97",
+              text: "Georgia",
+            },
+            {
+              uuid: "f8af98",
+              text: "Germany",
+            },
+            {
+              uuid: "f8af96",
+              text: "Bangladesh",
+            },
+          ],
+        },
+      },
+      {
+        id: 3,
+        type: "checkbox",
+        question: "From wich country are you?",
+        description: null,
+        data: {
+          options: [
+            {
+              uuid: "f8af96",
+              text: "USA",
+            },
+            {
+              uuid: "f8af97",
+              text: "Georgia",
+            },
+            {
+              uuid: "f8af98",
+              text: "Germany",
+            },
+            {
+              uuid: "f8af96",
+              text: "Bangladesh",
+            },
+          ],
+        },
+      },
+      {
+        id: 1,
+        type: "radio",
+        question: "From wich country are you?",
+        description: null,
+        data: {
+          options: [
+            {
+              uuid: "f8af96",
+              text: "USA",
+            },
+            {
+              uuid: "f8af97",
+              text: "Georgia",
+            },
+            {
+              uuid: "f8af98",
+              text: "Germany",
+            },
+            {
+              uuid: "f8af96",
+              text: "Bangladesh",
+            },
+          ],
+        },
+      },
+      {
+        id: 1,
+        type: "checkbox",
+        question: "From wich country are you?",
+        description: null,
+        data: {
+          options: [
+            {
+              uuid: "f8af96",
+              text: "USA",
+            },
+            {
+              uuid: "f8af97",
+              text: "Georgia",
+            },
+            {
+              uuid: "f8af98",
+              text: "Germany",
+            },
+            {
+              uuid: "f8af96",
+              text: "Bangladesh",
+            },
+          ],
+        },
+      },
+      {
+        id: 6,
+        type: "text",
+        question: "What's your favourite youtueb channel",
+        description: null,
+        data: {},
+      },
+      {
+        id: 7,
+        type: "textarea",
+        question: "What's your favourite youtueb channel",
+        description: null,
+        data: {},
+      },
+    ],
+  },
+];
+
 const store = createStore({
   state: {
     user: {
       data: {},
       token: sessionStorage.getItem("TOKEN"),
     },
+    surveys: [...tmpSurveys],
   },
   getters: {},
   actions: {
